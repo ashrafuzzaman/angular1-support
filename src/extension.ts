bc3d85c78db6cfc4b22c63ca4581898c71a8ff5a
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2017-07-31 12:56:40 +0600

    Get current word

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,11 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "angular1-support" is now active!');
     let disposable = vscode.commands.registerCommand('extension.gotoAngularDef', () => {
-        vscode.window.showInformationMessage('Hello World!');
+        const editor = vscode.window.activeTextEditor,
+            document = editor.document,
+            pos = editor.selection.active;
+        let selectedWord = document.getText(document.getWordRangeAtPosition(pos));
+        vscode.window.showInformationMessage('Hello World! ' + selectedWord);
     });
 
     context.subscriptions.push(disposable);
